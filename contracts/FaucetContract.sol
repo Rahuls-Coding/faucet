ae1cfd7b5a87c8aa10ef55e4aa7c473c52223813
Rahul <rahulscodingworld@gmail.com>
2022-07-30 14:32:06 +0530

    added migration js file and performed truffle migrate

---
+++ contracts/FaucetContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet { 
+    //storage variables
+    uint public funds = 1000; //uint is a 256bit unsigned integer that is required to be positive 
+    int public counter = -10; 
+    uint32 public test = 4294967295;
+    // address public owner = msg.sender\
+
+
+}
---
+++ migrations/2_faucet_migration.js
@@ -0,0 +1,7 @@
+
+const FaucetConract = artifacts.require('Faucet');
+
+
+module.exports = function(deployer) {
+    deployer.deploy(FaucetConract)
+    }
