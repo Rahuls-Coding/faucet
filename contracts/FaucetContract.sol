960f6680c3499967767ab2f47917200cf38df444
Rahul <rahulscodingworld@gmail.com>
2022-08-01 16:37:32 +0530

    some variables remvoed

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,9 +4,10 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet { 
     //storage variables
     uint public funds = 1000; //uint is a 256bit unsigned integer that is required to be positive 
-    int public counter = -10; 
-    uint32 public test = 4294967295;
     // address public owner = msg.sender\
 
+    //funds = 300
+    //funds = 1000
+    //funds = 2000
 
 }
